852acfeb4718e76c7464e0359a7c633e2dc27cba
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-05-01 02:11:09 -0500

    Update i2c.h

--- I2C_LCD/i2c.h
+++ I2C_LCD/i2c.h
@@ -1,17 +1,37 @@
-/* A stm32f103 library for user applications
- * Warren W. Gay VE3WWG
- * Sat Nov 25 11:53:53 2017
- */
+
+/************************************************************************
+* Copyright 2020 ITESM                                                  *
+*                                                                       *
+*                                                                       *
+* LCD_I2C                                                               *
+*                                                                       *
+* Autorhs:                                                              *
+* Jesús Enrique Luna Medina          A01632334                          *
+* Daniela abigail Parrales Mejía     A01228629                          *
+* Luis Cortés Leal                   A01631163                          *
+*                                                                       *
+* Abril 2020                                                            *
+* The project titled LCD_I2C is carried out with the purpose of         *
+* implementing a temperature sensing system which acquires the          *
+* values from the environment and delivers a response through           *
+* hardware using LEDs and software displaying on a Terminal.            *
+*                                                                       *
+************************************************************************/
+
+/*!< Conditions to confirm if macro-identifier is defined */
 #ifndef I2C_H
 #define I2C_H
-
 #include <stdint.h>
 #include <stdbool.h>
 
-enum I2C_RW {
+/***********************************************************************/
+
+enum I2C_RW{
     Read = 1,
     Write = 0
 };
+
+/***********************************************************************/
 
 void i2c_setup(void);
 void i2c_start_addr(uint8_t addr, enum I2C_RW rw);
@@ -21,6 +41,8 @@ void i2c_stop(void);
 void i2c_write(uint8_t byte);
 void i2c_write_8bits(uint8_t addr, uint8_t byte);
 
-#endif// I2C_H
+/***********************************************************************/
 
-// End i2c.h
+#endif
+
+/***********************************************************************/
