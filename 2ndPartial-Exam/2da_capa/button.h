b17aea2bdaaa4c055437a8655097ef870ebe65ee
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-05-04 21:47:29 -0500

    Update button.h

--- 2ndPartial-Exam/2da_capa/button.h
+++ 2ndPartial-Exam/2da_capa/button.h
@@ -1,10 +1,35 @@
+
+/************************************************************************
+* Copyright 2020 ITESM                                                  *
+*                                                                       *
+*                                                                       *
+* Second Partial Exam                                                   *
+*                                                                       *
+* Autorhs:                                                              *
+* Jesús Enrique Luna Medina          A01632334                          *
+* Daniela abigail Parrales Mejía     A01228629                          *
+* Luis Cortés Leal                   A01631163                          *
+* Victor Joseph Guerrero             A01375994                          *
+*                                                                       *
+* Mayo  2020                                                            *
+* This program is a cronometer which includes three buttons             *
+* in order to control the functions of: Reset, Start and Stop           *
+*                                                                       *
+************************************************************************/
+
+/*!< Conditions to confirm if macro-identifier is defined */
 #ifndef BUTTON_H
 #define BUTTON_H
-
 #include <stdint.h>
+
+/***********************************************************************/
 
 char BUTTON_START_READ(void);
 char BUTTON_STOP_READ(void);
 char BUTTON_RESET_READ(void);
 
+/***********************************************************************/
+
 #endif
+
+/***********************************************************************/
