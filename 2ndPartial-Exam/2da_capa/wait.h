9a849520278289c5a569e6985992762352374487
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-05-04 21:51:34 -0500

    Update wait.h

--- 2ndPartial-Exam/2da_capa/wait.h
+++ 2ndPartial-Exam/2da_capa/wait.h
@@ -1,6 +1,31 @@
+/************************************************************************
+* Copyright 2020 ITESM                                                  *
+*                                                                       *
+*                                                                       *
+* Second Partial Exam                                                   *
+*                                                                       *
+* Autorhs:                                                              *
+* Jesús Enrique Luna Medina          A01632334                          *
+* Daniela abigail Parrales Mejía     A01228629                          *
+* Luis Cortés Leal                   A01631163                          *
+* Victor Joseph Guerrero             A01375994                          *
+*                                                                       *
+* Mayo  2020                                                            *
+* This program is a cronometer which includes three buttons             *
+* in order to control the functions of: Reset, Start and Stop           *
+*                                                                       *
+************************************************************************/
+
+/*!< Conditions to confirm if macro-identifier is defined */
 #ifndef WAIT_H
 #define WAIT_H
 
+/***********************************************************************/
+
 void wait_s(int seconds);
 
+/***********************************************************************/
+
 #endif
+
+/***********************************************************************/
